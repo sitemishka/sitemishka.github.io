4c167a7fb6a8daf07cd1bd896e53cca70f7c7440
Irina <irbris@mail.ru>
2016-12-29 12:07:05 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,85 @@
+// Menu
+
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
+
+navMain.classList.remove("main-nav--nojs");
+
+navToggle.addEventListener("click", function() {
+	if (navMain.classList.contains("main-nav--closed")) {
+		navMain.classList.remove("main-nav--closed");
+		navMain.classList.add("main-nav--opened");
+	} else {
+		navMain.classList.add("main-nav--closed");
+		navMain.classList.remove("main-nav--opened");
+	}
+});
+
+// Map //
+
+var map = document.querySelector(".contacts__map");
+
+map.addEventListener("click", function(){
+    map.classList.remove("contacts__map--nojs");
+});
+
+
+
+
+// http://osvaldas.info/caching-svg-sprite-in-localstorage
+
+;( function( window, document )
+{
+    'use strict';
+
+    var file     = 'img/sprite.svg',
+        revision = 1;
+
+    if( !document.createElementNS || !document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ).createSVGRect )
+        return true;
+
+    var isLocalStorage = 'localStorage' in window && window[ 'localStorage' ] !== null,
+        request,
+        data,
+        insertIT = function()
+        {
+            document.body.insertAdjacentHTML( 'afterbegin', data );
+        },
+        insert = function()
+        {
+            if( document.body ) insertIT();
+            else document.addEventListener( 'DOMContentLoaded', insertIT );
+        };
+
+    if( isLocalStorage && localStorage.getItem( 'inlineSVGrev' ) == revision )
+    {
+        data = localStorage.getItem( 'inlineSVGdata' );
+        if( data )
+        {
+            insert();
+            return true;
+        }
+    }
+
+    try
+    {
+        request = new XMLHttpRequest();
+        request.open( 'GET', file, true );
+        request.onload = function()
+        {
+            if( request.status >= 200 && request.status < 400 )
+            {
+                data = request.responseText;
+                insert();
+                if( isLocalStorage )
+                {
+                    localStorage.setItem( 'inlineSVGdata',  data );
+                    localStorage.setItem( 'inlineSVGrev',   revision );
+                }
+            }
+        }
+        request.send();
+    }
+    catch( e ){}
+
+}( window, document ) );
